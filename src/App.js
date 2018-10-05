fe21dbc1280adfcc1b4eab456b35103e05285863
doriguzman <dorianguzman@ac.c4q.nyc>
2018-10-05 17:07:41 -0400

    finished current

--- src/App.js
+++ src/App.js
@@ -1,25 +1,101 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import axios from "axios";
+import CurrentWeather from "./CurrentWeather";
+import CurrentWeather2 from "./CurrentWeather2";
+import FiveDay from "./FiveDay";
 
-class App extends Component {
+class App extends React.Component {
+  constructor() {
+    super();
+    this.radioOptions = ["Kelvin", "Fahrenheit", "Celsius"];
+    this.state = {
+      submitted: "",
+      input: "",
+      city: "",
+      metric: "Fahrenheit",
+      unitFormat: "imperial"
+    };
+  }
+
+  handleInput = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+    if (e.target.value === "Fahrenheit") {
+      this.setState({
+        unitFormat: "imperial"
+      });
+    } else if (e.target.value === "Celsius") {
+      this.setState({
+        unitFormat: "metric"
+      });
+    } else if (e.target.value === "Kelvin") {
+      this.setState({
+        unitFormat: ""
+      });
+    }
+  };
+
+  handleSubmit = e => {
+    console.log("submitting");
+    e.preventDefault();
+    this.setState({
+      submitted: true,
+      city: this.state.input,
+      input: ""
+    });
+  };
+
   render() {
+    console.log(this.state);
+    const { input, city, current, metric, submitted, unitFormat } = this.state;
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="body-div">
+        <div className="search">
+          Your Location{" "}
+          <input
+            type="text"
+            name="input"
+            value={input}
+            onChange={this.handleInput}
+          />
+          <button type="submit" onClick={this.handleSubmit}>
+            {" "}
+            SUBMIT{" "}
+          </button>
+          <div className="metric">
+            Select metric:{" "}
+            {this.radioOptions.map(value => (
+              <span>
+                {" "}
+                {value}{" "}
+                <input
+                  type="radio"
+                  name="metric"
+                  value={value}
+                  checked={metric === value}
+                  onChange={this.handleInput}
+                />
+              </span>
+            ))}{" "}
+          </div>
+        </div>
+
+        {submitted ? (
+          <div>
+            <CurrentWeather2
+              city={city}
+              metric={metric}
+              unitFormat={unitFormat}
+            />
+            {/* <FiveDay city={city} metric={metric} unitFormat={unitFormat} /> */}
+          </div>
+        ) : (
+          ""
+        )}
+
+
       </div>
     );
   }
