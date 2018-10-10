9c2144052be21ca87bbb4323385b9da377bd53be
doriguzman <dorianguzman@ac.c4q.nyc>
2018-10-10 01:16:45 -0400

    top-bar and background css

--- src/Working/Weather.jsx
+++ src/Working/Weather.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import axios from "axios";
-import FiveDay from "./Components/FiveDay";
-import Current from "./Components/Current";
+import FiveDay from "../FiveDay";
+import Current from "../Current";
 import './Current.css'
 
 
